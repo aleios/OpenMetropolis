7a9bbd22a9c727d7c2617170215cef570f5fd589
aleios <dvsaleios@gmail.com>
2015-05-17 05:36:55 +1000

    Git Ignore

--- src/OpenMetropolis/GameScreen.cpp
+++ src/OpenMetropolis/GameScreen.cpp
@@ -1,4 +1,8 @@
 #include <OpenMetropolis/GameScreen.hpp>
+#include <OpenMetropolis/ScreenController.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Window/Event.hpp>
+#include <SFML/System/Time.hpp>
 
 GameScreen::GameScreen(std::shared_ptr<ScreenController> controller)
 	: Screen(controller)
@@ -6,21 +10,25 @@ GameScreen::GameScreen(std::shared_ptr<ScreenController> controller)
 }
 
 void GameScreen::OnEnter()
-{
-
+{   
 }
 
 void GameScreen::OnLeave()
 {
-
+    
 }
 
 void GameScreen::Update(const sf::Time& timeElapsed)
 {
-
+   auto& window = m_controller->GetWindow();
+   sf::Event ev;
+   while(window.pollEvent(ev))
+   {
+      if(ev.type == sf::Event::Closed)
+         m_controller->Quit();
+   }
 }
 
 void GameScreen::Draw()
 {
-
 }
